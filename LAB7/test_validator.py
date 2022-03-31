0fbe88e741de42c5381c25eb8f939925859aa849
Vladyslav Humennyy <humennyy.vlad@gmail.com>
2022-04-01 00:40:38 +0300

    LAB 7 2 2 tests fail

---
+++ LAB7/test_validator.py
@@ -0,0 +1,134 @@
+import unittest
+import validator
+
+class TestValidator(unittest.TestCase):
+    def test_name_surname(self):
+        """
+        Testing name and surname function
+        """
+        valid = validator.Validator()
+        # name and surname
+        assert valid.validate_name_surname("Elvis Presley") is True
+        # Not 2 words
+        assert valid.validate_name_surname("ElvisPresley") is False
+        assert valid.validate_name_surname("Elvis Presley forever") is False
+
+        # should be only first uppercase letter in name and surname
+        assert valid.validate_name_surname("elvis Presley") is False
+        assert valid.validate_name_surname("Elvis presley") is False
+        assert valid.validate_name_surname("Elvis PResley") is False
+
+        # size of both name and surname shoulb be between 2 and 30
+        assert valid.validate_name_surname("Elvis Presleyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq") is False
+        assert valid.validate_name_surname("Elvis P") is False
+
+        #no digits or punctuation in name or surname
+        assert valid.validate_name_surname("Elvis P,resley") is False
+        assert valid.validate_name_surname("El1vis Presley") is False
+
+    def test_age(self):
+        valid = validator.Validator()
+        # valid age id digit berween 16 and 99
+        assert valid.validate_age("20") is True
+        assert valid.validate_age("7") is False
+        assert valid.validate_age("100") is False
+        assert valid.validate_age("20.") is False
+        assert valid.validate_age("20a") is False
+
+    def test_country(self):
+        valid = validator.Validator()
+        # valid country - between 2 and 10 chars, first letter should be uppercase, can`t contain numbers
+        assert valid.validate_country("Ukraine") is True
+        assert valid.validate_country("U") is False
+        assert valid.validate_country("UUUUUUUUUUUUUUUUUUUUUUU") is False
+        assert valid.validate_country("Ukraine1") is False
+        assert valid.validate_country("ukraine") is False
+        assert valid.validate_country("USA") is True
+
+    def test_region(self):
+        valid = validator.Validator()
+        # valid region - the same as country, but can contain numbers
+        assert valid.validate_region("Lviv") is True
+        assert valid.validate_region("Lviv1") is True
+        assert valid.validate_region("L") is False
+        assert valid.validate_region("lviv") is False
+
+    def test_living_place(self):
+        valid = validator.Validator()
+        # living place - should be in format: "Koselnytska st. 2a"
+        # name of street - between 3 and 20 chars, first character uppercase, no digits in it
+        # type of street - should be "st.", "av.", "prosp." or "rd."
+        # number of building - exactly 2 symbols, first should be number, second can be number or small letter
+        assert valid.validate_living_place("Koselnytska st. 2a") is True
+        assert valid.validate_living_place("koselnytska st. 2a") is False
+        assert valid.validate_living_place("Koselnytska provulok 2a") is False
+        assert valid.validate_living_place("Koselnytska st. 2") is False
+        assert valid.validate_living_place("Koselnytska st. a2") is False
+        assert valid.validate_living_place("Koselnytska st. 22") is True
+
+    def test_index(self):
+        valid = validator.Validator()
+        # valid index - exactly 5 digits
+        assert valid.validate_index("79000") is True
+        assert valid.validate_index("7900") is False
+        assert valid.validate_index("790000") is False
+        assert valid.validate_index("7900q") is False
+        assert valid.validate_index("790 00") is False
+
+    def test_phone(self):
+        valid = validator.Validator()
+        # valid phone - in format "+380951234567" or "+38 (095) 123-45-67"
+        # starts wit "+" and has from 9 to 12 numbers
+        assert valid.validate_phone("+380951234567") is True
+        assert valid.validate_phone("+38 (095) 123-45-67") is True
+        assert valid.validate_phone("38 (095) 123-45-67") is False
+        assert valid.validate_phone("380951234567") is False
+        assert valid.validate_phone("-380951234567") is False
+        assert valid.validate_phone("+3810951234567") is False
+        assert valid.validate_phone("+20951234567") is True
+
+    def test_email(self):
+        valid = validator.Validator()
+        # valid email should be in format "username@domain.type"
+        # username - any letters, digits, any of "!#$%&'*+-/=?^_`{|}~", dots (provided that it is not the first or last
+        # character and provided also that it does not appear consecutively), at least 1, at most 64
+        # domain - only lowercase letters, at least 1, at most 255, but
+        # be careful - can be also "." - for example @ucu.edu.ua
+        # type : "com", "org", "edu", "gov", "net", "ua",....
+        assert valid.validate_email("username@domain.com") is True
+        assert valid.validate_email("username+usersurname@domain.com") is True
+        assert valid.validate_email("username@ucu.edu.ua") is True
+        assert valid.validate_email("usernamedomain.com") is False
+        assert valid.validate_email("username@domaincom") is False
+        assert valid.validate_email("username@domain.aaa") is False
+        assert valid.validate_email("username@aaa") is False
+        assert valid.validate_email("@domain.com") is False
+        assert valid.validate_email("myemail.@ukr.net") is False
+        assert valid.validate_email("python*is super@ukr.net") is False
+
+    def test_id(self):
+        valid = validator.Validator()
+        # valid id - exactly 6 digits, but should contain exactly one zero - at any position
+        assert valid.validate_id("123450") is True
+        assert valid.validate_id("011111") is True
+        assert valid.validate_id("123456") is False
+        assert valid.validate_id("123006") is False
+        assert valid.validate_id("1230916") is False
+        assert valid.validate_id("12306") is False
+
+    def test_split(self):
+        valid = validator.Validator()
+        assert valid.split_validate("Elvis Presley,20,Ukraine,Lviv,Koselnytska st. 2a,79000,+380951234567,username@domain.com,123450") == ["Elvis Presley","20","Ukraine","Lviv","Koselnytska st. 2a","79000","+380951234567","username@domain.com","123450"]
+
+    def test_general(self):
+        valid = validator.Validator()
+        # data - string in format "name_surname,age,country,region,living_place,index,phone,email,id"
+        # can also be whitespaces between sections and allowed separator ші ";"
+        # all previous criteria are valid
+        assert valid.validate("Elvis Presley,20,Ukraine,Lviv,Koselnytska st. 2a,79000,+380951234567,username@domain.com,123450") is True
+        assert valid.validate("Elvis Presley;20;Ukraine;Lviv;Koselnytska st. 2a;79000;+380951234567;username@domain.com;123450") is True
+        assert valid.validate("Elvis Presley; 20; Ukraine; Lviv; Koselnytska st. 2a; 79000; +380951234567; username@domain.com; 123450") is True
+        assert valid.validate("Elvis Presley, 20, Ukraine, Lviv, Koselnytska st. 2a, 79000, +380951234567, username@domain.com, 123450") is True
+
+if __name__ == "__main__":
+    unittest.main()
--- LAB7/validator.py
+++ LAB7/validator.py
@@ -1,152 +1,82 @@
+"""
+LAB 7 2
+"""
+
 import re
 
 class Validator:
+    """
+    The class for validator of registration data
+    """
     def validate_name_surname(self, name_surname: str):
-        return bool(re.search("^[A-Z][a-z]{1,29}\s[A-Z][a-z]{1,29}$" ,name_surname))
+        """
+        Function to validate data of name and surname
+        """
+        return bool(re.search("^[A-Z][a-z]{1,29}\s[A-Z][a-z]{1,29}$", name_surname))
 
     def validate_age(self, age: str):
-        try:
-            age = int(age)
-        except ValueError:
-            return False
-        return isinstance(age, int) and 15 < age < 100
+        """
+        Function to validate the age of a person
+        """
+        return bool(re.search("^\d\d$", age))
 
     def validate_country(self, country: str):
-        return country.isalpha() and country[0].isupper() and \
-    2 <= len(country) <= 10
+        """
+        Function to validate country of a person
+        """
+        return bool(re.search("^[A-Z][A-z]{1,9}$", country))
 
     def validate_region(self, region: str):
-        return region[0].isupper() and \
-               2 <= len(region) <= 10
+        """
+        Function to validate region of a person
+        """
+        return bool(re.search("^[A-Z][A-z,0-9]{1,9}$", region))
 
     def validate_living_place(self, living_place: str):
-        living_place = living_place.split()
-        return living_place[0].isalpha() and living_place[0][0].isupper() \
-    and 3 <= len(living_place[0]) <= 20 \
-    and living_place[1] in ["st.", "av.", "prosp." ,"rd."] \
-    and living_place[2][0].isnumeric() and len(living_place[2]) == 2 \
-    and (living_place[2][1].isnumeric()
-    or (living_place[2][1].isalpha() and living_place[2][1].islower()))
+        """
+        Function to validate living place of a person
+        """
+        return bool(re.search("^[A-Z][a-z]{2,19} (st.|av.|prosp.|rd.) \d(\d|[a-z])$", living_place))
 
     def validate_index(self, index: str):
-        return index.isnumeric() and len(index) == 5
+        """
+        Function to validate index of a person
+        """
+        return bool(re.search("^\d{5}$", index))
 
     def validate_phone(self, phone: str):
-        pass
+        """
+        Function to validate phone of a person
+        """
+        return bool(re.search("^\+\d{2}( \()?\d{3}(\) )?\d{0,3}(-?\d{2}){2}$", phone))
 
     def validate_email(self, email: str):
-        pass
+        """
+        Function to validate email of a person
+        """
+        return bool(re.search("^(?!\.)((?!\.\.).){1,64}(?!\.)@([a-z.]*\.(com|ua|org|net|edu|gov){,3}){1,255}$", email))
 
     def validate_id(self, id: str):
-        pass
+        """
+        Function to validate id of a person
+        """
+        return bool(re.search("^[0-9]{6}$", id)) and id.count("0") == 1
+
+    def split_validate(self, data):
+        """
+        Function to split the data of a person
+        """
+        spliters = [", ", "; ", ",", ";"]
+        data_temp = []
+        for spliter in spliters:
+            data_temp = data.split(spliter)
+            if len(data_temp) == 9:
+                return data_temp
 
     def validate(self, data: str):
-        pass
+        """
+        Function to validate the data of a person
+        """
+        functions = [self.validate_name_surname, self.validate_age, self.validate_country, self.validate_region,self.validate_living_place, self.validate_index, self.validate_phone, self.validate_email, self.validate_id]
+        return [functions[func](self.split_validate(data)[func]) for func in range(9)] == [True] * 9
 
-
-if __name__ == '__main__':
-    valid = Validator()
-
-    # name and surname
-    assert valid.validate_name_surname("Elvis Presley") is True
-    # Not 2 words
-    assert valid.validate_name_surname("ElvisPresley") is False
-    assert valid.validate_name_surname("Elvis Presley forever") is False
-
-    # should be only first uppercase letter in name and surname
-    assert valid.validate_name_surname("elvis Presley") is False
-    assert valid.validate_name_surname("Elvis presley") is False
-    assert valid.validate_name_surname("Elvis PResley") is False
-
-    # size of both name and surname shoulb be between 2 and 30
-    assert valid.validate_name_surname(
-        "Elvis Presleyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq") is False
-    assert valid.validate_name_surname("Elvis P") is False
-
-    # no digits or punctuation in name or surname
-    assert valid.validate_name_surname("Elvis P,resley") is False
-    assert valid.validate_name_surname("El1vis Presley") is False
-
-    # valid age id digit berween 16 and 99
-    assert valid.validate_age("20") is True
-    assert valid.validate_age("7") is False
-    assert valid.validate_age("100") is False
-    assert valid.validate_age("20.") is False
-    assert valid.validate_age("20a") is False
-
-    # valid country - between 2 and 10 chars, first letter should be uppercase, can`t contain numbers
-    assert valid.validate_country("Ukraine") is True
-    assert valid.validate_country("U") is False
-    assert valid.validate_country("UUUUUUUUUUUUUUUUUUUUUUU") is False
-    assert valid.validate_country("Ukraine1") is False
-    assert valid.validate_country("ukraine") is False
-    assert valid.validate_country("USA") is True
-
-    # valid region - the same as country, but can contain numbers
-    assert valid.validate_region("Lviv") is True
-    assert valid.validate_region("Lviv1") is True
-    assert valid.validate_region("L") is False
-    assert valid.validate_region("lviv") is False
-
-    # living place - should be in format: "Koselnytska st. 2a"
-    # name of street - between 3 and 20 chars, first character uppercase, no digits in it
-    # type of street - should be "st.", "av.", "prosp." or "rd."
-    # number of building - exactly 2 symbols, first should be number, second can be number or small letter
-    assert valid.validate_living_place("Koselnytska st. 2a") is True
-    assert valid.validate_living_place("koselnytska st. 2a") is False
-    assert valid.validate_living_place("Koselnytska provulok 2a") is False
-    assert valid.validate_living_place("Koselnytska st. 2") is False
-    assert valid.validate_living_place("Koselnytska st. a2") is False
-    assert valid.validate_living_place("Koselnytska st. 22") is True
-
-    # valid index - exactly 5 digits
-    assert valid.validate_index("79000") is True
-    assert valid.validate_index("7900") is False
-    assert valid.validate_index("790000") is False
-    assert valid.validate_index("7900q") is False
-    assert valid.validate_index("790 00") is False
-
-    # valid phone - in format "+380951234567" or "+38 (095) 123-45-67"
-    # starts wit "+" and has from 9 to 12 numbers
-    assert valid.validate_phone("+380951234567") is True
-    assert valid.validate_phone("+38 (095) 123-45-67") is True
-    assert valid.validate_phone("38 (095) 123-45-67") is False
-    assert valid.validate_phone("380951234567") is False
-    assert valid.validate_phone("-380951234567") is False
-    assert valid.validate_phone("+3810951234567") is False
-    assert valid.validate_phone("+20951234567") is True
-
-    # valid email should be in format "username@domain.type"
-    # username - any letters, digits, any of "!#$%&'*+-/=?^_`{|}~", dots (provided that it is not the first or last
-    # character and provided also that it does not appear consecutively), at least 1, at most 64
-    # domain - only lowercase letters, at least 1, at most 255, but
-    # be careful - can be also "." - for example @ucu.edu.ua
-    # type : "com", "org", "edu", "gov", "net", "ua",....
-    assert valid.validate_email("username@domain.com") is True
-    assert valid.validate_email("username+usersurname@domain.com") is True
-    assert valid.validate_email("username@ucu.edu.ua") is True
-    assert valid.validate_email("usernamedomain.com") is False
-    assert valid.validate_email("username@domaincom") is False
-    assert valid.validate_email("username@domain.aaa") is False
-    assert valid.validate_email("username@aaa") is False
-    assert valid.validate_email("@domain.com") is False
-
-    # valid id - exactly 6 digits, but should contain exactly one zero - at any position
-    assert valid.validate_id("123450") is True
-    assert valid.validate_id("011111") is True
-    assert valid.validate_id("123456") is False
-    assert valid.validate_id("123006") is False
-    assert valid.validate_id("1230916") is False
-    assert valid.validate_id("12306") is False
-
-    # data - string in format "name_surname,age,country,region,living_place,index,phone,email,id"
-    # can also be whitespaces between sections and allowed separator ші ";"
-    # all previous criteria are valid
-    assert valid.validate(
-        "Elvis Presley,20,Ukraine,Lviv,Koselnytska st. 2a,79000,+380951234567,username@domain.com,123450") is True
-    assert valid.validate(
-        "Elvis Presley;20;Ukraine;Lviv;Koselnytska st. 2a;79000;+380951234567;username@domain.com;123450") is True
-    assert valid.validate(
-        "Elvis Presley; 20; Ukraine; Lviv; Koselnytska st. 2a; 79000; +380951234567; username@domain.com; 123450") is True
-    assert valid.validate(
-        "Elvis Presley, 20, Ukraine, Lviv, Koselnytska st. 2a, 79000, +380951234567, username@domain.com, 123450") is True
