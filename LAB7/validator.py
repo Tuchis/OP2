3656904b769204ba1c1d5107f72f385c668c6d57
Vladyslav Humennyy <humennyy.vlad@gmail.com>
2022-04-01 11:29:44 +0300

    LAB 7 2 Right regex

--- LAB7/validator.py
+++ LAB7/validator.py
@@ -1,19 +1,21 @@
-# pylint: disable=line-too-long
 """
 LAB 7 2
 """
 
 import re
 
+
 class Validator:
     """
     The class for validator of registration data
     """
+
     def validate_name_surname(self, name_surname: str):
         """
         Function to validate data of name and surname
         """
-        return bool(re.search("^[A-Z][a-z]{1,29}\s[A-Z][a-z]{1,29}$", name_surname))
+        return bool(
+            re.search("^[A-Z][a-z]{1,29}\s[A-Z][a-z]{1,29}$", name_surname))
 
     def validate_age(self, age: str):
         """
@@ -37,7 +39,9 @@ class Validator:
         """
         Function to validate living place of a person
         """
-        return bool(re.search("^[A-Z][a-z]{2,19} (st.|av.|prosp.|rd.) \d(\d|[a-z])$", living_place))
+        return bool(
+            re.search("^[A-Z][a-z]{2,19} (st.|av.|prosp.|rd.) \d(\d|[a-z])$",
+                      living_place))
 
     def validate_index(self, index: str):
         """
@@ -49,19 +53,22 @@ class Validator:
         """
         Function to validate phone of a person
         """
-        return bool(re.search("^\+\d{2}( \()?\d{3}(\) )?\d{0,3}(-?\d{2}){2}$", phone))
+        return bool(
+            re.search("^\+\d{2}( \()?\d{3}(\) )?\d{0,3}(-?\d{2}){2}$", phone))
 
     def validate_email(self, email: str):
         """
         Function to validate email of a person
         """
-        return bool(re.search("^[^\s\.]{1}[^\s]{1,64}[^\s\.]{1}@([a-z.]*\.(com|ua|org|net|edu|gov){,3}){1,255}$", email)) and ".." not in email
+        return bool(re.search(
+            "^[^\s\.]{1}[^\s]{1,64}[^\s\.]{1}@([a-z.]*\.(com|ua|org|net|edu|gov){,3}){1,255}$",
+            email)) and ".." not in email
 
     def validate_id(self, given_id: str):
         """
         Function to validate id of a person
         """
-        return bool(re.search("^[0-9]{6}$", given_id)) and given_id.count("0") == 1
+        return bool(re.search("^((?=^\d{6}$)[1-9]{,5}0[1-9]{,5}|f)$", given_id))
 
     def split_validate(self, data):
         """
@@ -78,6 +85,10 @@ class Validator:
         """
         Function to validate the data of a person
         """
-        functions = [self.validate_name_surname, self.validate_age, self.validate_country, self.validate_region,self.validate_living_place, self.validate_index, self.validate_phone, self.validate_email, self.validate_id]
-        return [functions[func](self.split_validate(data)[func]) for func in range(9)] == [True] * 9
-
+        functions = [self.validate_name_surname, self.validate_age,
+                     self.validate_country, self.validate_region,
+                     self.validate_living_place, self.validate_index,
+                     self.validate_phone, self.validate_email,
+                     self.validate_id]
+        return [functions[func](self.split_validate(data)[func]) for func in
+                range(9)] == [True] * 9
