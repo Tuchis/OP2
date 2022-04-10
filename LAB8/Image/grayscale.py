a7bc5bc8a9ca7ddb5af7b34a2c9b3fa8f0af01ec
Vladyslav Humennyy <humennyy.vlad@gmail.com>
2022-04-11 01:17:47 +0300

    LAB 8 3 Decoding

--- LAB8/Image/grayscale.py
+++ LAB8/Image/grayscale.py
@@ -2,6 +2,7 @@
 MODULE DOCSTRING
 """
 import sys
+import math
 
 import numpy
 import numpy as np
@@ -85,15 +86,23 @@ class GrayscaleImage():
         len_ent = 256
         compressed = []
         value = tuple()
+        entries[np.ravel(self.photo)[0], np.ravel(self.photo)[1]] = len_ent
+        len_ent += 1
         for number in np.ravel(self.photo):
             if value + (number, ) in entries:
                 value = value + (number, )
             else:
                 compressed.append(entries[value])
-                entries[value + (number, )] = len(entries)
+                if len_ent == 2 ** 16:
+                    len_ent = 256
+                if len(entries) - len_ent >= 256:
+                    del entries[list(entries.keys())[len_ent]]# 179 175 174 171 168 167 166 164 163 162
+                entries[value + (number, )] = len_ent
                 len_ent += 1
                 value = (number, )
-        return np.array(compressed, dtype='uint16')
+        compressed += value
+        self.compressed = np.array(compressed, dtype='uint16')
+        return self.compressed
 
 
     def lzw_decompression(self):
@@ -104,10 +113,52 @@ class GrayscaleImage():
         """
         entries = {}
         for i in range(256):
-            entries[(i,)] = i
-        len_ent = 256
-        decompressed = []
-        # for
+            entries[i] = (i, )
+        entries_cursor = 257
+        entries[256] = (171, 168)
+        decompressed = tuple()
+        value = self.compressed[0]
+        decompressed += entries[int(value)]
+        value = entries[int(value)]
+
+        def unpack(entries_local):
+            result = ()
+            for elem_local in entries_local:
+                if elem_local > 255:
+                    result += (unpack(entries[elem_local]),)
+                else:
+                    result += (int(elem_local),)
+            return result
+
+        for i in range(1, len(self.compressed)):
+            new = int(self.compressed[i])
+            if new in entries:
+                entry = entries[new]
+                if isinstance(entry, tuple) or isinstance(entry, np.ndarray):
+                    for elem in entry:
+                        if elem > 255:
+                            decompressed += unpack(entries[int(elem)])
+                        else:
+                            decompressed += (int(elem), )
+                else:
+                    if entry > 255:
+                        decompressed += (int(entries[int(entry)]),)
+                    else:
+                        decompressed += (int(entry),)
+                add = value + (entry[0], ) if isinstance(entry, tuple) else (entry, )
+                value = entry
+            else:
+                entry = value + (value[0], )
+                decompressed += entry
+                for elem in entry:
+                    if elem >= 255:
+                        print(entry)
+                add = entry
+                value = entry
+            entries[entries_cursor] = add
+            entries_cursor += 1
+        return np.array(decompressed, dtype='uint8').reshape((self.height(), self.width()))
+
 
 def main():
     """
@@ -131,8 +182,13 @@ def main():
           photo.photo.size)
     print("Memory size of one array element in bytes: ",
           photo.photo.itemsize)
-    print("The memoru usage is : ", photo.photo.itemsize * photo.photo.size)
+    print("The memory usage is : ", photo.photo.itemsize * photo.photo.size)
     print("------------")
+    print("Changing some values ot have better tests")
+    photo.photo[0][3] = 175
+    photo.photo[0][4] = 175
+    photo.photo[0][5] = 175
+    photo.photo[0][6] = 175
     print("Compressing the photo")
     compressed = photo.lzw_compression()
     print("Array: ",compressed)
@@ -140,6 +196,20 @@ def main():
     print("Size of the compressed array: ", sys.getsizeof(compressed))
     print("The difference between uncompressed and compressed: ", sys.getsizeof(photo.photo) - sys.getsizeof(compressed))
     print("Ratio of compressing: ", sys.getsizeof(compressed) / sys.getsizeof(photo.photo) * 100, "%")
+    #Decompressing
+    print("-------------")
+    print("Decompressing of the photo")
+    decompressed = photo.lzw_decompression()
+    print(photo)
+    print(decompressed)
+    print(len(decompressed))
+    print("Size of the array: ",
+          decompressed.size)
+    print("Memory size of one array element in bytes: ",
+          decompressed.itemsize)
+    print("The memoru usage is : ", decompressed.itemsize * decompressed.size)
+    print(decompressed == photo.photo)
+    print("------------")
 
 if __name__ == "__main__":
     main()
